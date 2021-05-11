a0c37a8e30be464b9ed5098be011b9919a262743
Yisrael Gurkow <yisrelg3@gmail.com>
2021-05-11 14:48:23 -0400

    form for adding trx is working

--- index.js
+++ index.js
@@ -4,18 +4,64 @@ const assetsDiv = document.getElementById('assets');
 const totalDiv = document.getElementById('total');
 const expensesForm = document.getElementById('expenses-form'); 
 const assetsForm = document.getElementById('assets-form');
-const newTransactForm = document.querySelector('#new-transaction-form')
-addTransaction()
+
+
 function updateTotals() {
     const expenseTotal = expensesDiv.totalExpenseValue.value
     const assetTotal = expensesDiv.totalAssetValue.value
     const grandTotal = parseInt(expenseTotal) + parseInt(assetTotal)
 } 
 
-function addTransaction (item) {
+function showFinances(){
+    fetch('http://localhost:3000/finances')
+        .then(res => res.json())
+        .then((financeArr)=>{
+            financeArr.forEach(financeObj => {
+                let div
+                if (financeObj.description === 'asset'){
+                    div = document.createElement('div');
+                    div.dataset.id = financeObj.id
+                    div.innerHTML = `
+                    <h3>Name of Asset: </h3>
+                    <h1> ${financeObj.name} </h1>
+                    <h3>Value: </h3>
+                    <h1 class='itemValue'> ${financeObj.value} </h1>
+                    `
+                    assetsDiv.append(div)
+                }
+                else{
+                    div = document.createElement('div');
+                    div.dataset.id = financeObj.id
+                    div.innerHTML = `
+                    <h3>Name of Expense: </h3>
+                    <h1> ${financeObj.name} </h1>
+                    <h3>Value: </h3>
+                    <h1 class='itemValue'> ${financeObj.value} </h1>
+                    `
+                    expensesDiv.append(div)
+                }
+               
+                div.addEventListener('click',(event) => {
+                    newTransaction.innerHTML = `
+                        <form id = 'new-transaction-form' data-id='${financeObj.id}'>
+                            <label for="newValue">New Transaction: </label><br>
+                            <input type="text" id="newValue" name="newValue" placeholder="Transaction Amount">
+                            <input type="submit" value="Submit">
+                        </form>`
+                 addTransaction(financeObj)
+                })
+            });
+        })
+}
+
+showFinances();
+
+function addTransaction (financeObj) {
+    const newTransactForm = document.querySelector('#new-transaction-form')
     newTransactForm.addEventListener ('submit', (event) => {
         event.preventDefault()
-        const newTransactionValue = event.target.newValue.value
+        const newTransactionValue = financeObj.value + (parseInt(event.target.newValue.value) || 0)
+        //console.log(newTransactionValue)
         fetch(`http://localhost:3000/finances/${newTransactForm.dataset.id}`,
             {method: "PATCH",
             headers:{"Content-Type" : "application/json"},
@@ -23,6 +69,9 @@ function addTransaction (item) {
             }
         )
         .then(res => res.json())
-        .then(amount => console.log(amount.value))
+        .then((newValue) => {
+            document.querySelector(`div[data-id = "${financeObj.id}"] .itemValue`).innerText = newValue.value
+            newTransactForm.innerHTML = ''
+        })
     })
 }
