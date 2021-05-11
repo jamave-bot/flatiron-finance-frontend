68cd09d38b6ea1d680b0df7f056ca9e2bbd2fb78
jimmyalvarez <jimmyalvarezjr@gmail.com>
2021-05-11 16:27:51 -0400

    fetch function

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const totalDiv = document.getElementById('total');
 const expensesForm = document.getElementById('expenses-form'); 
 const assetsForm = document.getElementById('assets-form');
 
-
+let newExpenseInput = document.querySelector('input#expenseName')
+let valueOfExpense = docuemnt.querySelector('input#totalExpenseValue')
 
 let newAssetInput = document.querySelector('input#assetsName')
 let valueOfAsset = document.querySelector('input#totalAssetValue')
@@ -14,22 +15,34 @@ assetsForm.addEventListener('submit', (evt) => {
     evt.preventDefault();
     let newAssetInput = document.querySelector('input#assetsName').value
     let valueOfAsset = parseInt(document.querySelector('input#totalAssetValue').value)
-
-    fetch(`http://localhost:3000/finances`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            asset: newAssetInput,
-            description: 'asset',
-            value: valueOfAsset
-        }),
-        })
-        .then((res) => res.json())
-        .then(res => {
-
-        });
-
-
+    fetchPost(newAssetInput, valueOfAsset)
 })
+
+    
+expensesForm.addEventListener('submit', (evt) => {
+    evt.preventDefault()
+    let newExpense = newExpenseInput.value
+    let newExpenseValue = valueOfExpense.value
+    fetchPost(newExpense, newExpenseValue)
+})
+
+
+
+    
+function fetchPost(input, valueofInput){
+ fetch(`http://localhost:3000/finances`, {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+        asset: input,
+        description: 'asset',
+        value: valueofInput
+    }),
+    })
+    .then((res) => res.json())
+    .then(res => {
+
+    })
+}
