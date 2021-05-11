d31ee319e7352a6881b8ba132525a24cc90d7657
jimmyalvarez <jimmyalvarezjr@gmail.com>
2021-05-11 15:06:29 -0400

    post

--- index.js
+++ index.js
@@ -7,3 +7,27 @@ const assetsForm = document.getElementById('assets-form');
 
 
 
+
+
+assetsForm.addEventListener('submit', (evt) => {
+evt.preventDefault
+let newAssetInput = assetInput.value
+let valueOfAsset = assetValue.value
+
+fetch(`http://localhost:3000//${evt.target.dataset.id}`, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+      asset: newAssetInput
+      value: valueOfAsset
+  }),
+})
+  .then((res) => r.json())
+  .then(res => {
+
+  });
+
+
+})
